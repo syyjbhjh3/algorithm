452cbadf7b6f8b6907d02be40c1293afb7341f03
Young Jun Park <syyjbhjh7@gmail.com>
2022-11-17 18:04:45 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static boolean check[];
+    public static int ans = 0;
+
+    public int solution(int k, int[][] dungeons) {
+        check = new boolean[dungeons.length];
+
+        dfs(k, dungeons, 0);
+
+        return ans;
+    }
+    public static void dfs(int tired, int[][] dungeons, int cnt){
+
+        for(int i=0; i<dungeons.length; i++){
+            if(!check[i] && dungeons[i][0]<=tired){
+                check[i] = true;
+                dfs(tired-dungeons[i][1], dungeons, cnt+1);
+                check[i] = false;
+            }
+        }
+        ans = Math.max(ans, cnt);
+    }
+}
