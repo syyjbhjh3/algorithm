7cb2d73f267d88c422087ef14f5a3019f934d758
Young Jun Park <syyjbhjh7@gmail.com>
2023-03-07 15:48:50 +0900

    [unrated] Title: 덧칠하기, Time: 1.33 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,11 @@
+function solution(n, m, section) {
+  let answer = 0;
+  let max = 0;
+  section.forEach((s) => {
+    if (s > max) {
+      answer++;
+      max = s + m - 1;
+    }
+  });
+  return answer;
+}
