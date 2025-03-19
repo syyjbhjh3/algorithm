0a8a4c2aa992408ce227d3fb26cd0e1fadde303d
Young Jun Park <syyjbhjh7@gmail.com>
2025-03-20 00:00:36 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 768 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,9 @@
+# 연립방정식 (#19532)
+
+a, b, c, d, e, f = map(int, input().split())
+
+for i in range(-999, 1000):
+    for j in range(-999, 1000):
+        if a * i + b * j == c and d * i + e * j == f:
+            print(i, j)
+            break
